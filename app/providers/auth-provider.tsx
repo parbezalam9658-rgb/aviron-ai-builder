c390f11f839347a565cc66a6aed15f66b5fee582
Parbez Alam <parbezalam9658@gmail.com>
2026-05-09 11:10:58 +0530

    fixed authLoading

--- app/providers/auth-provider.tsx
+++ app/providers/auth-provider.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export function useAuth() {
   return {
     user: null,
-    loading: false,
+    authLoading: false,
     role: "user",
     plan: "free",
     isAdmin: false,
